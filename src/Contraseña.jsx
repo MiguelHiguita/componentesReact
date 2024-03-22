d3d507f286fafd5dc5fa71cd25ed3509efdd2abf
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-03-22 11:02:54 -0500

    crear repo

---
+++ src/Contraseña.jsx
@@ -0,0 +1,10 @@
+function Contraseña(){
+    return(
+        <div class="input-group">
+          <label for="password">Contraseña:</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+    )
+}
+
+export default Contraseña
